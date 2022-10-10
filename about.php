89647d4ff719647f88962def8ffc8d797a33edc8
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2022-10-10 00:56:19 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Provisio Resort - ABOUT</title>
+<link rel="stylesheet"   href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+<?php require('inc/links.php'); ?>
+
+</head>
+
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+      <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Laudantium aliquid provident <br> fugit et maxime consequuntur 
+      hic accusantium voluptatibus repellat quas.
+    </p>
+    </div> 
+    <div class="col-lg-5 ">
+        
+    </div>
+
+    <div class="container">
+        <div class="row">
+           <div class="col-lg-6">
+             <h3 class="mb-3">Lorem ipsum dolor sit.</h3> 
+             <p>
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                 Cupiditate enim pariatur, quo velit a minima accusantium!
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                 Cupiditate enim pariatur, quo velit a minima accusantium!
+             </p> 
+           </div> 
+        </div>
+    </div>
+  
+    <?php require('inc/footer.php'); ?>
+
+
+</body>
+</html>
--- facilities.php
+++ facilities.php
@@ -4,9 +4,16 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Provisio Resort -HOME</title>
+<title>Provisio Resort - FACILITIES</title>
 <?php require('inc/links.php'); ?>
-<link rel="stylesheet"   href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+<style>
+  .pop:hover{
+   border-top-color: var(--teal) !important;
+   transform:scale(1.03);
+   transition: all 0.3s;
+  }
+</style>
+
 </head>
 
 <body class="bg-light">
@@ -15,8 +22,95 @@
 
     <div class="my-5 px-4">
       <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
-<hr>
-    <div class="h-line"></div>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Laudantium aliquid provident <br> fugit et maxime consequuntur 
+      hic accusantium voluptatibus repellat quas.
+    </p>
+    </div> 
+
+    <div class="container"> 
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div> 
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+            <img src="images/features/wifi.svg" width="40px">
+            <h5 class= "m-0 ms-3">Wifi</h5>  
+              </div>
+             <p> 
+             We provide our guests with Google Fiber
+             providing speeds of up to 2,000 Mbps at all of our
+             luxurious  locations.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   
     <?php require('inc/footer.php'); ?>
